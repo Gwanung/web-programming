9814e6e48b70d48d5ee9dcaa5f26275502f4eb34
com11-133\OWNER <rhksdnd457@gmail.com>
2022-08-09 16:46:10 +0900

    css 및 java 공부용

---
+++ 2022.08 study/java/2022.08.09_Java/ch03/src/EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner input = new Scanner(System.in);
+		System.out.print("정수를 입력하세요: ");
+		number = input.nextInt();
+		
+		if(number % 2 == 0) {  //짝수인가
+			System.out.println("입력된 정수는 짝수입니다.");
+		} else {
+			System.out.println("입력된 정수는 홀수입니다.");
+		}
+		
+		System.out.println("프로그램이 종료되었습니다.");
+		
+	}
+
+}

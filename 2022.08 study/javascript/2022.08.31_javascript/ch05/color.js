881184faf0d7528d2da92169ada7d359dba438fe
Gwanung <ktx7943@naver.com>
2022-08-31 17:42:59 +0900

    Java 및 js 공부용
    token date / jquery 연습

---
+++ 2022.08 study/javascript/2022.08.31_javascript/ch05/color.js
@@ -0,0 +1,31 @@
+var Body = {
+    setColor:function(color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+} 
+
+var Links = {
+    setColor:function(color){
+        var alist = document.querySelectorAll('a');
+        for(var i=0; i<alist.length; i++){
+            alist[i].style.color = color;
+        }
+    }
+}
+function nightDayHandler(self){  //중복되는 것들은 함수를 통해서 반복되는 입력을 줄임. 
+    var target = document.querySelector('body');  
+    if(self.value === 'night'){  
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setColor('powderblue');
+    }else{
+        Body.setBackgroundColor("white");
+        Body.setColor('black');
+        self.value = 'night';
+        Links.setColor('blue');
+    }
+} //nightDayHandler()

9a389cb6e5ee0f6d27efe55d53d0c64addbaabf8
Gwanung <ktx7943@naver.com>
2022-10-21 21:05:40 +0900

    jsp webmarket, vue props

---
+++ 2022.10.study/vue/Card.vue
@@ -0,0 +1,21 @@
+<template>
+   <div v-for="(a,i) in oneroom" :key="i">
+    <img :src="a.image" alt="" class="room-img">
+    <h4 @click="$emit('openModal', oneroom.id)" p_number= "i">{{a.title}}</h4>
+    <p>{{a.price}}원</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'ThisCard',
+    props : {
+    oneroom : Array,
+    p_number : Number,    
+    }
+}
+</script>
+
+<style>
+
+</style>

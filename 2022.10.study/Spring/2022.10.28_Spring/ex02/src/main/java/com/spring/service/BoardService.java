d2b784ad1a444af09227919bc8b7404d3d5aa5b3
Gwanung <ktx7943@naver.com>
2022-10-28 21:14:41 +0900

    Spring  화면처리 및 CRUD 구현

---
+++ 2022.10.study/Spring/2022.10.28_Spring/ex02/src/main/java/com/spring/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.spring.service;
+
+import java.util.List;
+
+import com.spring.domain.BoardVO;
+
+public interface BoardService {
+	
+	public void register(BoardVO vo);
+	public BoardVO get(Long bno);
+	public int modify(BoardVO vo);
+	public int remove(Long bno);
+	public List<BoardVO> getList();
+
+}
